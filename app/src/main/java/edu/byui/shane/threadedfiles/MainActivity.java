b97a16a80094f9cc57161b8f6615058c1cfecf01
fieryshadow <jen13077@byui.edu>
2015-06-05 18:13:10 -0600

    GUI layout and implementation for file creation, loading, and list clearing.

--- app/src/main/java/edu/byui/shane/threadedfiles/MainActivity.java
+++ app/src/main/java/edu/byui/shane/threadedfiles/MainActivity.java
@@ -1,17 +1,43 @@
 package edu.byui.shane.threadedfiles;
 
+import android.app.Activity;
+import android.content.Context;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.LogRecord;
 
 
 public class MainActivity extends ActionBarActivity {
+    private ProgressBar mProgress;
+    private List<String> loadedLines;
+    private ArrayAdapter<String> viewLines;
+    private Handler mHandler = new Handler();
+    private final String filename = "numbers.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mProgress = (ProgressBar) findViewById(R.id.progressBar);
+        loadedLines = new ArrayList<>();
+        viewLines = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, loadedLines);
+        ((ListView) findViewById(R.id.listView)).setAdapter(viewLines);
     }
 
     @Override
@@ -34,5 +60,97 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void showToast(final String toast) {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void updateListView() {
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                viewLines.notifyDataSetChanged();
+            }
+        });
+    }
+
+    public void createFile(View view) {
+        final Context context = getApplicationContext();
+        mProgress.setProgress(0);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File f = new File(context.getFilesDir(), filename);
+                    if (!f.exists()) {
+                        f.createNewFile();
+                    }
+                    BufferedWriter file = new BufferedWriter(new java.io.FileWriter(f));
+
+                    for (int i = 1; i <= 10; i++) {
+                        file.write(i + "\n");
+                        mHandler.post(new Runnable() {
+                            public void run() {
+                                mProgress.incrementProgressBy(10);
+                            }
+                        });
+                        Thread.sleep(250);
+                    }
+                    file.close();
+                    showToast("File creation successful.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    showToast("Error creating file.");
+                }
+            }
+        }).start();
+    }
+
+    public void loadFile(View view) {
+        final Context context = getApplicationContext();
+        mProgress.setProgress(0);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    File f = new File(context.getFilesDir(), filename);
+                    if (!f.exists()) {
+                        throw new IOException();
+                    }
+                    BufferedReader file = new BufferedReader(new java.io.FileReader(f));
+
+                    for (int i = 1; i <= 10; i++) {
+                        loadedLines.add(file.readLine());
+                        mHandler.post(new Runnable() {
+                            public void run() {
+                                mProgress.incrementProgressBy(10);
+                            }
+                        });
+                        Thread.sleep(250);
+                    }
+                    file.close();
+                    updateListView();
+                    showToast("File loaded.");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    showToast("Error loading file.");
+                }
+            }
+        }).start();
+    }
+
+    public void clearList(View view) {
+        mProgress.setProgress(0);
+        viewLines.clear();
     }
 }
